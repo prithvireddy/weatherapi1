e852971965d540a777deefe48f4feadca047661b
Prithvi Reddy <PrithviReddy@truckstop.com>
2021-02-07 18:23:34 -0500

    Modified main.tf .yaml

--- main.tf
+++ main.tf
@@ -4,38 +4,29 @@ provider "azurerm" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name = "tf_rg_blobstore"
-    storage_account_name = "tfstorageaccount11"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "tf_test" {
-  name = "tf_rg_blobstore"
+  name = "tfmainrg"
   location = "East US"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-  name      =  "weatherapi"
-  location  =   azurerm_resource_group.tf_test.location
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
   resource_group_name = azurerm_resource_group.tf_test.name
 
   ip_address_type = "public"
-  dns_name_label = "weatherapi"
+  dns_name_label = "weatherapidns"
   os_type = "Linux"
 
   container {
-    name   = "weatherapi"
-    image  = "prithvireddy9/dotnetsample:latest"
-    cpu    = "0.5"
+    name = "weatherapi"
+    image = "prithvireddy9/dotnetsample"
+    cpu =  "1"
     memory = "1"
-  
+
     ports {
-      port     = 80
+      port = 80
       protocol = "TCP"
-        }
-  } 
+    }
+  }
 }
